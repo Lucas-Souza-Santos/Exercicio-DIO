7d2848156bc9058e49cdda9b0593d17f88371d6b
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-02-17 03:14:11 -0300

    primeiro exercicio completo

---
+++ SolucoesJavaScriptAvancado/ex01.js
@@ -0,0 +1,52 @@
+let N = 7
+let numeros = [8, 10, 8, 260, 4, 10, 10]
+// let numeros = [8, 8, 8, 8, 8, 8, 8, 8]
+
+let numerosOrd = numeros.sort((a, b) => {
+    return a - b;
+})
+
+const unicos1 = numerosOrd.filter((valor, index, self) => {
+    return self.indexOf(valor) === index;
+})
+
+console.log(unicos1);
+
+
+
+let N = gets();
+let numeros = []
+
+for(let i = 0; i < N; i++)
+{
+  numeros.push(parseInt(gets()))
+}
+
+const ordenarNumero = numeros.sort((a, b) => {
+  return a - b;
+})
+
+const valoresUnicos = ordenarNumero.filter((valor, index, self) => {
+    return self.indexOf(valor) === index;
+})
+
+// outra maneira de criar um vetor com valores unico
+// const unicos2 = [...new Set(numerosOrd)]
+// console.log(numerosOrd)
+// console.log(unicos2)
+
+const valores = ordenarNumero.reduce((valor, item) => {
+    if(!valor[item]) valor[item] = 1;
+    
+    else valor[item]++;
+
+    return valor
+}, [])
+
+let cont = 0
+
+for(const key in valores)
+{
+  console.log(valoresUnicos[cont] + " aparece " + valores[key] + " vez(es)")
+  cont++;
+}
